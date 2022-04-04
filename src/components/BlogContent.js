f41c6f207b8f8afc606985db2dc007293a5558ec
Craig Ford <craig_ford@icloud.com>
2022-04-04 08:34:18 -0400

    Completed

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 function BlogContent(props) {
+  console.log('in blogcontent ', props);
   return <div id="blog-content">{props.articleText}</div>;
 }
 
--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 function Comment(props) {
+  console.log('in Comments ', props);
   return <div className="comment">{props.commentText}</div>;
 }
 
